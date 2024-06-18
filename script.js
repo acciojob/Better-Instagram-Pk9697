b0d34ea8cd9c0f13ed2c488e7d23cdf6dca4ad93
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-18 18:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function drag(e) {
+	// console.log(e.target.backgroundImage)
+  e.dataTransfer.setData("text", e.target.id);
+}
+
+function drop(e) {
+  e.preventDefault();
+  var data = e.dataTransfer.getData("text");
+  // e.target.appendChild(document.getElementById(data));
+	const ele=e.target
+	e.target=document.getElementById(data)
+	// document.getElementById(data)=ele
+}
