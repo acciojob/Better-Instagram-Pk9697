ca130806368c11bdc44d7cedca288af8bea0627e
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-07-02 21:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 //your code here
 function allowDrop(e) {
 	e.preventDefault();
-}
-
+} 
+ 
 function drag(e) {
-	// console.log(e.target.backgroundImage)
 	e.dataTransfer.setData("text", e.target.id);
-}
-
+} 
+  
 function drop(e) {
 	e.preventDefault();
 	const data = e.dataTransfer.getData("text");
-	const draggedElement=document.getElementById(data)
+	const draggedElementParent=document.getElementById(data)
 	const droppedOnElement=e.target
-  
-	console.log(draggedElement,droppedOnElement)
-	  
-	// Save the sibling of the element we dragged because we need this for the insertBefore() method
-	  const draggedElementSibling = draggedElement.nextSibling === droppedOnElement ? draggedElement : draggedElement.nextSibling;
-	 
-	  // Swap elements 
-	  droppedOnElement.parentNode.insertBefore(draggedElement, droppedOnElement.nextSibling);
-	  droppedOnElement.parentNode.insertBefore(droppedOnElement, draggedElementSibling);
-	
-	
+	const droppedOnElementParent=e.target.parentNode
+  	
+	const temp=draggedElementParent.childNodes[1].id
+	draggedElementParent.childNodes[1].id=droppedOnElementParent.childNodes[1].id
+	droppedOnElementParent.childNodes[1].id=temp	
 }
